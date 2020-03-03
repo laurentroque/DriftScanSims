b8a70c7dcca6d2f66c15749f30c6c42eb27cb47a
Ian <laurent.roque@yahoo.com>
2020-03-03 17:03:39 +0000

    added initial values to main script

--- ta_reach.py
+++ ta_reach.py
@@ -1,6 +1,7 @@
 # REACH Drift Scan Simulations
 # Import stuff
 import numpy
+import scipy.io.loadmat as load
 
 # Time and Frequency Range
 obsFreq = numpy.arange(50, 151)*1E+6 # Observation frequency in Hz
@@ -13,3 +14,16 @@ rot = 0 # Antenna rotation in degrees
 # Sky maps and antenna beam directories in l-m plane
 pdir = DriftScanSims.npy_beam_dipoleandbalun # Beam directory
 mdir = DriftScanSims.ta_REACHmaps # Map directory
+
+# Load sky maps (LFSM) 40 - 250MHz
+maps = load('maps_LFSM_40to250M_3deg.mat') # High resolution LFSM maps
+mapf_index = numpy.arange(40, 250)*1E+6 # Map frequency index (Shouldn't have to change)
+
+# Initial values
+d2r = numpy.pi / 180
+r2d = 1.0 / d2r
+res = 512 # Resolution for all maps and beams (512x512), 512==0.35deg
+ra = numpy.linspace(360, 0, maps.shape[1]) # Right Ascension in degrees
+dec = numpy.linspace(-90, 90, maps.shape[0]) # Declination in degrees
+RAi, DECi = numpy.meshgrid(ra, dec) 
+^^^^ check this is correct
