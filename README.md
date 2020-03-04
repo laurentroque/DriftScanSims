377e1be290994389c138bc7b6cd68e24102a3d53
Ian <laurent.roque@yahoo.com>
2020-03-04 17:53:26 +0000

    Initial variables added

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## About
 This code is a drift scan simulator based on the MATLAB code written by Dr. Nima Razavi-Ghods (University of Cambridge). 
 ## Dependencies
+Large matrix such as maps_LFSM_10Mto250M_3deg.mat for the sky maps etc will have to be obtained by [Nima](nima@mrao.cam.ac.uk) as they are large (2GB).
--- ta_reach.py
+++ ta_reach.py
@@ -1,7 +1,7 @@
 # REACH Drift Scan Simulations
 # Import stuff
 import numpy
-import scipy.io.loadmat as load
+from scipy.io import loadmat as load
 
 # Time and Frequency Range
 obsFreq = numpy.arange(50, 151)*1E+6 # Observation frequency in Hz
@@ -12,11 +12,12 @@ fref = 50E+6 # Reference frequency to calculate chromaticity function
 rot = 0 # Antenna rotation in degrees
 
 # Sky maps and antenna beam directories in l-m plane
-pdir = DriftScanSims.npy_beam_dipoleandbalun # Beam directory
-mdir = DriftScanSims.ta_REACHmaps # Map directory
+pdir = 'npy_beam_dipoleandbalun' # Beam directory
+mdir = 'ta_REACHmaps' # Map directory
 
 # Load sky maps (LFSM) 40 - 250MHz
-maps = load('maps_LFSM_40to250M_3deg.mat') # High resolution LFSM maps
+maps = load('maps_LFSM_40Mto250M_3deg.mat') # High resolution LFSM maps
+maps = maps['maps']
 mapf_index = numpy.arange(40, 250)*1E+6 # Map frequency index (Shouldn't have to change)
 
 # Initial values
@@ -25,5 +26,19 @@ r2d = 1.0 / d2r
 res = 512 # Resolution for all maps and beams (512x512), 512==0.35deg
 ra = numpy.linspace(360, 0, maps.shape[1]) # Right Ascension in degrees
 dec = numpy.linspace(-90, 90, maps.shape[0]) # Declination in degrees
-RAi, DECi = numpy.meshgrid(ra, dec) 
-^^^^ check this is correct
+RAi, DECi = numpy.meshgrid(ra, dec)
+
+# Coordinates of REACH 5 site
+Lat = -30.836511
+Long = 21.370872
+
+Tsky = numpy.zeros((len(obsFreq), len(LST)))
+
+if cf == 1:
+  CF = numpy.zeros((len(obsFreq), len(LST)))
+
+# Frequency index
+for fidx in range(0, len(obsFreq)):
+  # Load antenna beams
+  beam_name = pdir + '/reach_hexdip_pat_' + str(int(obsFreq[fidx]/1E+6)) + '.mat'
+  beam = load(beam_name)['Pat']
